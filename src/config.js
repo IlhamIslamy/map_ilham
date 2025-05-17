f250bba40a7c1e14e9c7e50069181d9c6f3bf584
mohammad ilham <mohammad250503@gmail.com>
2025-05-18 00:09:48 +0700

    map_ilham

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  BASE_URL: 'https://story-api.dicoding.dev/v1',
+  MAPBOX_TOKEN: 'isi-token-mapbox-kamu-jika-perlu',
+  VAPID_PUBLIC_KEY: 'BM_IYEXqR0l3a9IudBg5Xi19jD7AYFPw6DT841IN4szGUxLW2kmShuy9eweaSdg2uy_9m_xC0pRgI6_EOTraHAA', // Ganti dengan key kamu
+};
+
+export default CONFIG;
